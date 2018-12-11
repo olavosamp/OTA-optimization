202ba7ad08e258283adbf308e621129a35859486
olavosamp <olavosamp@poli.ufrj.br>
2018-12-11 15:58:18 -0200

    Improved plots and response characterization

--- tests/characterization.py
+++ tests/characterization.py
@@ -3,9 +3,12 @@ import pandas                   as pd
 import matplotlib.pyplot        as plt
 
 from libs.cost_function    import transistor_response
+import libs.defines             as defs
+import libs.dirs                as dirs
+
 
 numPoints = 1e4
-delta = 0
+delta = -0.025
 x = np.linspace(-0.2+delta, 0.2+delta, num=numPoints)
 y = transistor_response(x, -delta)
 
@@ -42,15 +45,43 @@ span = np.abs(x[-1] - x[0])
 print(mean)
 # print(maxVal)
 
-print("Response Span: ", span)
+print("\nResponse Span: {:5.2e} V".format( span))
 
 # Plot response
-plt.plot(x, y, 'b')
+fig = plt.figure(figsize=(20,12))
+plt.plot(x, y, 'b', label='Sinal')
+plt.title("Resposta do Par Diferencial com atraso {:.2e}".format(delta))
 
 # Plot a vertical line through maxVal
-plt.axvline(x=x[xMaxValIndex], color='r')
+plt.axvline(x=x[xMaxValIndex], color='r', label='Ponto Máximo')
 
 # Plot horizontal line on mean value
-plt.axhline(y=mean, color='g')
+plt.axhline(y=mean, color='g', label='Valor Médio')
 
+
+# Filter Bandwidth
+threshold = maxVal*0.2
+bwIndex = np.argwhere(y >= threshold)
+
+bandwidth = np.squeeze(x[bwIndex[-1]] - x[bwIndex[0]])
+
+# Plot bandwidth limits
+plt.axvline(x=x[bwIndex[0] ], color='k', label='Limites de Banda')
+plt.axvline(x=x[bwIndex[-1]], color='k')
+plt.legend()
+
+# Annotations
+plt.annotate('{:.2e}'.format(np.squeeze(y[xMaxValIndex])), xy=(x[xMaxValIndex], y[xMaxValIndex]),
+              xytext=(2,4), textcoords='offset points')
+plt.annotate('{:.2e}'.format(np.squeeze(y[xMeanIndex[0]])), xy=(x[xMeanIndex[0]], y[xMeanIndex[0]]),
+              xytext=(100,4), textcoords='offset points')
+plt.annotate('{:.2e}'.format(np.squeeze(y[bwIndex[0]])), xy=(x[bwIndex[0]], y[bwIndex[0]]),
+              xytext=(-48,0), textcoords='offset points')
+plt.annotate('{:.2e}'.format(np.squeeze(y[bwIndex[-1]])), xy=(x[bwIndex[-1]], y[bwIndex[-1]]),
+              xytext=(2,0), textcoords='offset points')
+
+print("Bandwidth: {:12.2e} V".format( bandwidth))
+
+plt.savefig(dirs.figures+"response_characterization.png", orientation='portrait',
+            bbox_inches='tight')
 plt.show()
--- tests/sum_function_test.py
+++ tests/sum_function_test.py
@@ -10,32 +10,37 @@ M     = 12               # Number of transistor responses
 span  = 0.08             # Non-zero response width
 edge  = (M-1)/2*span
 delta = np.linspace(-edge,edge, num=M)
-# print(delta.shape)
-# exit()
-
-# M=5
-# start = -(M-1)/2*span
-# ---|---|---0---|---|---
-# M=6
-# start = -(M-1)/2*span
-# ---|---|---|-0-|---|---|---
 
 lowerBound = delta[0]  -0.5
 upperBound = delta[-1] +0.5
 pointDensity = 1e4/(0.4)
 
 
-x = np.linspace(lowerBound, upperBound, num=pointDensity*span)
+x = np.linspace(lowerBound, upperBound, num=round(pointDensity*span))
 
 y = sum_function(x, delta, M=M)
 
-print(x.shape)
-print(y.shape)
-fig = plt.plot(x,y)
+print("x ", x.shape)
+print("y ", y.shape)
+
+fig = plt.figure(figsize=(20,20))
+plt.plot(x,y)
 plt.xlim(-edge-span*2, +edge+span*2)
 
-plt.xlabel("V")
-plt.ylabel("gm")
+plt.xlabel("Tensão (V)")
+plt.ylabel("Transcondutância (gm)")
 # plt.show()
 
 plt.savefig(dirs.figures+"response_sum.png")
+
+
+# Obtain parameters
+maxVal    = np.max(y)
+dropIndex = np.argwhere(y > 0.2*maxVal)
+
+bandwidth = np.squeeze(x[dropIndex[-1]] - x[dropIndex[0]])
+yBW = y[dropIndex]
+ripple = np.max(yBW) - np.min(yBW)
+
+print("Bandwidth: ", bandwidth)
+print("Ripple: ", ripple)
