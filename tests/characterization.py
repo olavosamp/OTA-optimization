df95d6649af4005242fe73b86748b1ec8f622ca9
Olavo <olavosamp@poli.ufrj.br>
2018-12-10 22:01:45 -0200

    A bit more characterization work

--- tests/characterization.py
+++ tests/characterization.py
@@ -4,18 +4,53 @@ import matplotlib.pyplot        as plt
 
 from functions.cost_function    import transistor_response
 
-x = np.linspace(-0.2, 0.2, num=1e4)
-y = transistor_response(x, 0)
+numPoints = 1e4
+delta = 0
+x = np.linspace(-0.2+delta, 0.2+delta, num=numPoints)
+y = transistor_response(x, -delta)
 
-dataY = pd.DataFrame(y)
-print(dataY.describe())
+data = {'x': x,
+        'y': y}
 
-mean = dataY.mean(axis=0)
-max = dataY.max()
+dataDf = pd.DataFrame(data)
+maxVal = dataDf.max()['y']
 
+# Filter very small values
+threshold = maxVal*0.001
+newYIndex = np.argwhere(y >= threshold)
+y = np.squeeze(y[newYIndex])
+x = np.squeeze(x[newYIndex])
+
+# Create a new DataFrame with filtered x and y
+data = {'x': x,
+        'y': y}
+
+dataDf = pd.DataFrame(data)
+
+xMaxValIndex  = np.argmax(y)
+print(dataDf['y'].describe())
+
+mean = dataDf.mean(axis=0)['y']
 print(mean)
-print(max)
+# input()
+xMeanIndex = np.squeeze(np.argwhere(np.isclose(y, mean, atol=1e-10)))
+
+# print(y)
+# print(xMeanIndex)
 # exit()
-#
-# plt.plot(x, y)
+print(mean)
+# print(maxVal)
+
+# Get non-zero points
+
+plt.plot(x, y, 'b')
+
+# Plot a vertical line through maxVal
+plt.axvline(x=x[xMaxValIndex], color='r')
+
+plt.axhline(y=mean, color='g')
+# Plot a vertical line through Mean
+# for index in xMeanIndex:
+#     plt.axvline(x=x[index], color='g')
+
 plt.show()
